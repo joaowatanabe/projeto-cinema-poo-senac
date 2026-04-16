042eda8aab96246340497efb01e754a32bfd212e
wabemusic <jvwatanabe@gmail.com>
2026-04-16 19:53:51 -0300

    feat: cadastrarIngresso.ts

--- src/services/CadastrarIngresso.ts
+++ src/services/CadastrarIngresso.ts
@@ -1,7 +1,28 @@
 import { Ingresso } from "../entities/Ingresso";
+import { ClienteCinema } from "../entities/ClienteCinema";
+import { SessaoFilme } from "../entities/SessaoFilme";
+
+const PRECO_CHEIO = 25.0;
+const PRECO_MEIA = PRECO_CHEIO / 2;
 
 export class CadastrarIngresso {
-  private ingresso: Ingresso[] = []
+  private ingresso: Ingresso[] = [];
 
-  if ()
+  public emitirIngresso(
+    id: number,
+    cliente: ClienteCinema,
+    sessao: SessaoFilme,
+    assento: number,
+  ): Ingresso | null {
+    if (cliente.idade < sessao.filme.classificacao) {
+      console.log(
+        `\nErro: Cliente não tem idade mínima para este filme (${sessao.filme.classificacao} anos).\n`,
+      );
+      return null;
+    }
+  }
+
+  if(!sessao.assentoDisponível(assento)) {
+
+  }
 }
