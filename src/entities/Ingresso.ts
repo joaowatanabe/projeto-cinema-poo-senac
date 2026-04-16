e7b78ca6baca497ab0dff597399b105bdc35388f
wabemusic <jvwatanabe@gmail.com>
2026-04-16 19:39:45 -0300

    feat: ingresso corrigido ingresso.cliente e ingresso.assento

--- src/entities/Ingresso.ts
+++ src/entities/Ingresso.ts
@@ -2,12 +2,26 @@ import { ClienteCinema } from "./ClienteCinema";
 import { SessaoFilme } from "./SessaoFilme";
 
 export class Ingresso {
+  id: number;
+  cliente: ClienteCinema;
+  sessao: SessaoFilme;
+  assento: number;
+  meiaEntrada: boolean;
+  valorPago: number;
+
   constructor(
     id: number,
     cliente: ClienteCinema,
     sessao: SessaoFilme,
     assento: number,
-    meiaEntrada: ClienteCinema['estudante'],
+    meiaEntrada: boolean,
     valorPago: number,
-  ) {}
+  ) {
+    this.id = id;
+    this.cliente = cliente;
+    this.sessao = sessao;
+    this.assento = assento;
+    this.meiaEntrada = meiaEntrada;
+    this.valorPago = valorPago;
+  }
 }
