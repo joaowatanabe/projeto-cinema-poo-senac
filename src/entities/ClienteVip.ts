f25fe63bb0186af5dd256d0c2a9955420bf04f4d
wabemusic <jvwatanabe@gmail.com>
2026-05-21 10:15:20 -0300

    cliente vip

--- src/entities/ClienteVip.ts
+++ src/entities/ClienteVip.ts
@@ -10,22 +10,27 @@ export class ClienteVip extends ClienteCinema {
     this._anual = anual;
   }
 
+  public descricao(): string {
+    const plano = this._anual
+      ? "Anual"
+      : this._mensalidade
+        ? "Mensal"
+        : "Inativo";
+    return `[${this._id}] ${this._nome} | VIP Plano: ${plano}`;
+  }
+
   public get mensalidade(): boolean {
     return this._mensalidade;
   }
-
   public set mensalidade(valor: boolean) {
     this._mensalidade = valor;
   }
-
   public get anual(): boolean {
     return this._anual;
   }
-
   public set anual(valor: boolean) {
     this._anual = valor;
   }
-
   public get planoAtivo(): boolean {
     return this._mensalidade || this._anual;
   }
