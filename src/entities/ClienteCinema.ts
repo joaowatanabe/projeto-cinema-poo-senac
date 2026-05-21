195f75f094f53d77023afbfeffe422c1fadc5996
wabemusic <jvwatanabe@gmail.com>
2026-05-21 10:06:23 -0300

    feat: cliente cinema polimorfismo

--- src/entities/ClienteCinema.ts
+++ src/entities/ClienteCinema.ts
@@ -1,4 +1,4 @@
-export class ClienteCinema {
+export abstract class ClienteCinema {
   protected _id: number;
   protected _nome: string;
   protected _cpf: string;
@@ -15,46 +15,38 @@ export class ClienteCinema {
     this._estudante = false;
   }
 
+  public abstract descricao(): string;
+
   public get id(): number {
     return this._id;
   }
-
   public set id(id: number) {
     this._id = id;
   }
-
   public get nome(): string {
     return this._nome;
   }
-
   public set nome(nome: string) {
     this._nome = nome;
   }
-
   public get cpf(): string {
     return this._cpf;
   }
-
   public get idade(): number {
     return this._idade;
   }
-
   public set idade(idade: number) {
     this._idade = idade;
   }
-
   public get ativoVip(): boolean {
     return this._ativoVip;
   }
-
   public set ativoVip(ativoVip: boolean) {
     this._ativoVip = ativoVip;
   }
-
   public get estudante(): boolean {
     return this._estudante;
   }
-
   public set estudante(estudante: boolean) {
     this._estudante = estudante;
   }
