422d6960de28f3e7eccc7d79466a6fd1316ab785
wabemusic <jvwatanabe@gmail.com>
2026-05-21 10:08:02 -0300

    feat: Filme polimorfismo, cada subclasse descreve uma classificacao

--- src/entities/Filme.ts
+++ src/entities/Filme.ts
@@ -1,6 +1,6 @@
 import { SessaoFilme } from "./SessaoFilme";
 
-export class Filme {
+export abstract class Filme {
   protected _id: number;
   protected _titulo: string;
   protected _classificacao: number;
@@ -10,7 +10,7 @@ export class Filme {
   protected _sessoes: SessaoFilme[];
 
   constructor(id: number, titulo: string) {
-    this._id = id; 
+    this._id = id;
     this._titulo = titulo;
     this._classificacao = 0;
     this._duracaoMinutos = 0;
@@ -19,60 +19,48 @@ export class Filme {
     this._sessoes = [];
   }
 
+  public abstract descricao(): string;
+
   public get id(): number {
     return this._id;
   }
-
   public set id(id: number) {
     this._id = id;
   }
-
   public get titulo(): string {
     return this._titulo;
   }
-
   public set titulo(titulo: string) {
     this._titulo = titulo;
   }
-
   public get classificacao(): number {
     return this._classificacao;
   }
-
   public set classificacao(classificacao: number) {
     this._classificacao = classificacao;
   }
-
   public get sinopse(): string {
     return this._sinopse;
   }
-
   public set sinopse(sinopse: string) {
     this._sinopse = sinopse;
   }
-
   public get duracaoMinutos(): number {
     return this._duracaoMinutos;
   }
-
-  public set duracaoMinutos(duracaoMinutos: number) {
-    this._duracaoMinutos = duracaoMinutos;
+  public set duracaoMinutos(d: number) {
+    this._duracaoMinutos = d;
   }
-
-  public set ativo(ativo: boolean) {
-    this._ativo = ativo;
-  }
-
   public get ativo(): boolean {
     return this._ativo;
   }
-
+  public set ativo(ativo: boolean) {
+    this._ativo = ativo;
+  }
   public get sessoes(): SessaoFilme[] {
     return this._sessoes;
   }
-
   public set sessoes(sessoes: SessaoFilme[]) {
     this._sessoes = sessoes;
   }
 }
-
